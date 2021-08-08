b3399fad55c29984bb95b70c0efd88a9837fa798
anfebnBotz <88554978+anfebnBotz@users.noreply.github.com>
2021-08-09 01:15:48 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,11 +7,14 @@ handler.before =async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    m.reply('* ANTI LINK *\n\nTerdeteksi *${await this.getname(m.sender)}* Kamu Telah Mengirim link Grup!\n\Maaf Kamu Akan Dikick Dari Grup Ini Byee!`)
-    if (global.opts['restrict']) {
-      if (isAdmin || !isBotAdmin) return true
-      // this.groupRemove(m.chat, [m.sender])
-    }
+    await m.reply('* ANTI LINK *\n\nTerdeteksi *${await this.getname(m.sender)}* Kamu Telah Mengirim link Grup!\n\Maaf Kamu Akan Dikick Dari Grup Ini Byee!`)
+    if (isAdmin) return m.reply('*Eh Maaf kamu Admin, kamu gk bakal dikick. hehe..*')
+    if (!isBotAdmin) return m.reply('*Bot Bukan Admin, Mana bisa kick orang _-*')
+    let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
+    let isLinkThisGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkThisGc.test(m.text)
+    if (isgclink) return m.reply('*Lol ngirim link group sendiri :v*')
+    await this.groupRemove(m.chat, [m.sender])
   }
   return true
 }
