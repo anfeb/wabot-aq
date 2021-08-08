13a683a57ea181f3324c5db575d5e234348ac41a
anfebnBotz <88554978+anfebnBotz@users.noreply.github.com>
2021-08-09 00:12:34 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,13 +1,13 @@
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-handler.before = function (m, { isAdmin, isBotAdmin }) {
-  if (m.isBaileys && m.fromMe) return true
+handler.before =async function (m, { user, isBotAdmin, isAdmin }) {
+  if (m.isBaileys && m.fromMe) || m.fromMe || !m.isgroup) return true
   let chat = global.db.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    m.reply('Hapus!!\n\nLink Grup terdeteksi')
+    m.reply('* ANTI LINK *\n\nTerdeteksi *${await this.getname(m.sender)}* Kamu Telah Mengirim link Grup!\n\Maaf Kamu Akan Dikick Dari Grup Ini Byee!`)
     if (global.opts['restrict']) {
       if (isAdmin || !isBotAdmin) return true
       // this.groupRemove(m.chat, [m.sender])
